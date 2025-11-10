e204d86508b8e53724c90078225b8da59933cf0b
pedroHackerNoob <114507828+pedroHackerNoob@users.noreply.github.com>
2025-11-10 15:51:16 -0700

    get all updated

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ def get_cities():  # put application's code here
     print('get from /cities')
     for c in cities:
         print(c.name)
-    return jsonify([c.name for c in cities])
+    return jsonify([c.name for c in cities]),200
 @app.post('/cities')
 def post_cities():
     data = request.get_json()
