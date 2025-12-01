045606f29036aeb5d46368e2ca87d7e6965372b8
pedroHackerNoob <114507828+pedroHackerNoob@users.noreply.github.com>
2025-11-30 22:31:43 -0700

    beauty bar created

--- app.py
+++ app.py
@@ -1,64 +1,25 @@
 from flask import Flask, render_template, request, jsonify
 
-from entities.city import get_all_city, City
-from entities.customer import get_all_customer, Customer
-
 app = Flask(__name__)
 
-@app.route('/cities')
-def get_cities():  # put application's code here
-    cities = get_all_city()
-    print('get from /cities')
-    for c in cities:
-        print(c.name)
-    return jsonify([c.name for c in cities]),200
-@app.post('/cities')
-def post_city():
-    data = request.get_json()
-    c = City(name=data['name'])
-    id = c.post_city()
-    print(id)
-    succes = id is not None
-    return jsonify(succes), 201
-@app.put('/cities/<int:id>')
-def put_city(id):
-    print('put from /cities')
-    data = request.get_json()
-    c = City(id_city=id, name=data['name'])
-    c.put_city()
-    return jsonify(True), 201
-@app.delete('/cities/<int:id>')
-def delete_city(id):
-    c = City(id_city=id)
-    c.delete_city()
-    return jsonify(True), 200
-@app.route('/customer')
-def get_customer():
-    print('get from /customer')
-    customer = get_all_customer()
-    for c in customer:
-        print(c.name)
-    return jsonify([c.name for c in customer]),200
-@app.post('/customer')
-def post_customer():
-    data = request.get_json()
-    c = Customer(name=data['name'],email=data['email'],phone=data['phone'],zip=data['zip'])
-    id = c.post_customer()
-    print(id)
-    succes = id is not None
-    return jsonify(succes), 201
-@app.put('/customer/<int:id>')
-def put_customer(id):
-    print('put from /customer')
-    data = request.get_json()
-    c = Customer(id_customer=id, name=data['name'],email=data['email'],phone=data['phone'],zip=data['zip'])
-    c.put_customer()
-    return jsonify(True), 201
-@app.delete('/customer/<int:id>')
-def delete_customer(id):
-    print('delete from /customer')
-    c = Customer(id_customer=id)
-    c.delete_customer()
-    return jsonify(True), 200
+@app.route('/user')
+def home():  # put application's code here
+    return render_template('services.html')
+@app.route('/user/style')
+def services():
+    return render_template('stylist.html')
+
+@app.route('/user/style/confirm')
+def xd():
+    return render_template('confirmation.html')
+
+@app.route('/mod')
+def scheduleMod():
+    return render_template('modSchedule.html')
+
+@app.route('/admin')
+def admin():
+    return render_template('adminPanel.html')
+
 if __name__ == '__main__':
     app.run()
