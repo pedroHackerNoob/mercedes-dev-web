19254db16b9e67e454eb0d52559fe391fd9086eb
Tyler Brock <pedrohardbass@gmail.com>
2025-12-06 00:21:06 -0700

    new css added

--- app.py
+++ app.py
@@ -2,24 +2,9 @@ from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 
-@app.route('/user')
+@app.route('/')
 def home():  # put application's code here
-    return render_template('services.html')
-@app.route('/user/style')
-def services():
-    return render_template('stylist.html')
-
-@app.route('/user/style/confirm')
-def xd():
-    return render_template('confirmation.html')
-
-@app.route('/mod')
-def scheduleMod():
-    return render_template('modSchedule.html')
-
-@app.route('/admin')
-def admin():
-    return render_template('adminPanel.html')
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run()
